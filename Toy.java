433f9652f60f7a58ad743208f093239fcec2ddd4
600020344 <77069279+600020344@users.noreply.github.com>
2021-03-16 09:15:58 -0700

    Update Toy.java

--- Toy.java
+++ Toy.java
@@ -9,3 +9,49 @@
 //and sample runner
 //code provided
 //to help you
+public class Toy
+{
+  private String name;
+  private int count;
+
+  public Toy()
+  {
+    name = "";
+    count = 1;
+  }
+  public Toy(String n)
+  {
+    name = n;
+    count = 1;
+  }
+  public Toy(int c)
+  {
+    name = "";
+    count = c;
+  }
+  public Toy(String n, int c)
+  {
+    name = n;
+    count = c;
+  }
+  public void setName(String n)
+  {
+    name = n;
+  }
+  public void setCount(int c)
+  {
+    count = c;
+  }
+  public String getName()
+  {
+    return name;
+  }
+  public int getCount()
+  {
+    return count;
+  }
+  public String toString()
+  {
+    return name + " " + count;
+  }
+}
