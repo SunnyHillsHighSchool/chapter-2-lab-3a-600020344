fd32439dca2b234dae544ccb76117b84f8262566
600020344 <77069279+600020344@users.noreply.github.com>
2021-03-16 09:21:46 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,5 +6,15 @@ public class Main
 {
 	public static void main(String[] args)
 	{
+    Toy t = new Toy( "sorry" );
+    System.out.println( t.getCount() );
+    System.out.println( t );
+    System.out.println( t.getName() );
+
+    Toy s = new Toy( "ji goe" );
+    System.out.println(s.getCount() );
+    s.setCount( 5 );
+    System.out.println(s.getCount() );
+    System.out.println( s );
 	}
 }
